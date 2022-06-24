ee734bd7221d57fcbc8f519bec711927a997dc18
alagappan17 <alagappan1703@gmail.com>
2022-06-24 22:59:42 +0530

    Workbook 2.2

---
+++ Section-4/PersonalBio.java
@@ -0,0 +1,13 @@
+public class PersonalBio {
+    public static void main(String args[]) {
+        String name = "Alagappan";
+        String college = "Sri Venkateswara College of Engineering";
+        int year = 3;
+        String degree = "B.Tech";
+        String department = "Information Technology";
+        char section = 'A';
+        System.out.println("Hi, I'm " + name + " pursuing my " + degree + " in " + department + " at " + college
+                + ". I am currently in my " + year + "rd year.");
+    }
+
+}
