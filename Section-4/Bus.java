7e9fb72bc5af707d24fc4f87a6e8c15d4e53df79
alagappan17 <alagappan1703@gmail.com>
2022-06-24 22:43:23 +0530

    Java Strings

--- Section-4/Bus.java
+++ Section-4/Bus.java
@@ -1,7 +1,12 @@
 public class Bus {
     public static void main(String[] args) {
+        String sentence = "Number of passengers: ";
         int passengers = 0;
         passengers += 9;
-        System.out.println(passengers);
+        System.out.println(sentence + passengers);
+        passengers -= 5;
+        System.out.println(sentence + passengers);
+        passengers -= 2;
+        System.out.println(sentence + passengers);
     }
 }
---
+++ Section-4/JoiningStrings.java
@@ -0,0 +1,7 @@
+public class JoiningStrings {
+    public static void main(String args[]){
+        String greeting = "Hello Mr.";
+        String name = "Jack";
+        System.out.println(greeting + name);
+    }
+}
