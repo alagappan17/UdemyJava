e34c97757e3592a0c52a6e7f76ccb8841132d2e6
alagappan17 <alagappan1703@gmail.com>
2022-06-25 11:55:12 +0530

    Double Variables

---
+++ Section-4/Decimals.java
@@ -0,0 +1,14 @@
+public class Decimals {
+    public static void main(String args[]) {
+        double percentage = 55.7;
+        System.out.println("Percentage: " + percentage);
+
+        int dividend1 = 25;
+        int divisor1 = 2;
+        System.out.println("int divide: " + dividend1 / divisor1);
+
+        double dividend = 25;
+        double divisor = 2;
+        System.out.println("double  divide: " + dividend/ divisor);
+    }
+}
