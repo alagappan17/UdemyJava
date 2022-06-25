61cabfc10669d17b042e63ed74df3231da0f6757
alagappan17 <alagappan1703@gmail.com>
2022-06-25 11:46:45 +0530

    long variables

---
+++ Section-4/WholeNumbers.java
@@ -0,0 +1,8 @@
+public class WholeNumbers {
+    public static void main(String ars[]) {
+        int numberOfFan = 5000;
+        System.out.println("There are " + numberOfFan + " at the stadium.");
+        long globalPopulation = 7000000000L;
+        System.out.println("There are " + globalPopulation + " people in the world.");
+    }
+}
