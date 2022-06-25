65331036129d91556640529fefe2ff181c3f2722
alagappan17 <alagappan1703@gmail.com>
2022-06-25 12:50:35 +0530

    Type Conversion

---
+++ Section-4/TypeConversion.java
@@ -0,0 +1,7 @@
+public class TypeConversion {
+    public static void main(String[] args) {
+        double decimal = 69.42;
+        int newNumber = (int) decimal;
+        System.out.println(newNumber);
+    }
+}
