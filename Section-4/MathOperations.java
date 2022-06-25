177034fe324e0f233a0dcb36a7fe318817377e92
alagappan17 <alagappan1703@gmail.com>
2022-06-25 12:32:58 +0530

    Math Operations

---
+++ Section-4/MathOperations.java
@@ -0,0 +1,19 @@
+public class MathOperations {
+    public static void main(String[] args) {
+        int bagOfSweets = 5;
+        int bagOfSweets2 = 10;
+
+        long starsInMilkyWay = 2500000000L;
+        long starsInAndromeda = 1000000000000L;
+
+        double testScore = 6.7;
+        double bonusMarks = 2.5;
+
+        System.out.println("Fred and George collected " + (bagOfSweets + bagOfSweets2) + " sweets");
+        System.out.println("There are " + (starsInMilkyWay + starsInAndromeda)
+                + " stars in the Milky Way and Andromeda galaxies.");
+        System.out.println("With bonus marks, Ron scored " + (testScore + bonusMarks)
+                + " on his potions test. Snape wasn't too thrilled");
+
+    }
+}
