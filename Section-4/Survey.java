f4f10f92d59da36c133f7651c83465a46a038a36
alagappan17 <alagappan1703@gmail.com>
2022-06-25 13:42:20 +0530

    Scanner updated

--- Section-4/Survey.java
+++ Section-4/Survey.java
@@ -3,17 +3,23 @@ import java.util.Scanner;
 public class Survey {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        int counter = 0;
         System.out.println("What is your name?");
         String name = scan.nextLine();
+        counter++;
 
         System.out.println("How much do you spend on coffee?");
         double coffee = scan.nextDouble();
+        counter++;
 
         System.out.println("How many times a week do you buy coffee?");
         int coffeeTimes = scan.nextInt();
+        counter++;
 
+        System.out.println("Thank you for answering " + counter + " questions!");
         System.out.println("Name: " + name);
         System.out.println("Money spent on coffee: Rs." + coffee);
         System.out.println("Times a week: " + coffeeTimes);
+        scan.close();
     }
 }
