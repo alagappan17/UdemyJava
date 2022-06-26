89a3b8e47d0bd9fbdd66cfdac2e6615174798025
alagappan17 <alagappan1703@gmail.com>
2022-06-26 11:39:57 +0530

    Boolean Comparisions

---
+++ Section-6/BooleanComparisions.java
@@ -0,0 +1,13 @@
+public class BooleanComparisions {
+    public static void main(String[] args) {
+        int grade1 = 10;
+        int grade2 = 12;
+        int grade3 = 12;
+        System.out.println(grade1 > grade2);
+        System.out.println(grade1 < grade2);
+        System.out.println(grade3 <= grade2);
+        System.out.println(grade3 >= grade2);
+        System.out.println(grade3 == grade2);
+        System.out.println(grade3 != grade2);
+    }
+}
