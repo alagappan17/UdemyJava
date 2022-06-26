cb2d03b241478f9b7fdac75c73dc1d01e0748bfe
alagappan17 <alagappan1703@gmail.com>
2022-06-26 11:43:05 +0530

    String Comparisions

--- Section-6/BooleanComparisions.java
+++ Section-6/BooleanComparisions.java
@@ -9,5 +9,9 @@ public class BooleanComparisions {
         System.out.println(grade3 >= grade2);
         System.out.println(grade3 == grade2);
         System.out.println(grade3 != grade2);
+
+        String sentance1 = "Hello";
+        String sentance2 = "World";
+        System.out.println(!sentance1.equals(sentance2));
     }
 }
