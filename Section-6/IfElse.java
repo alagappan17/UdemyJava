de753019f6729ca048e7c308988644a95eed170e
alagappan17 <alagappan1703@gmail.com>
2022-06-26 12:06:57 +0530

    Switch Statements

---
+++ Section-6/IfElse.java
@@ -0,0 +1,9 @@
+public class IfElse {
+    public static void main(String args[]) {
+        int grade = 35;
+        if (grade > 40)
+            System.out.println("Passed");
+        else
+            System.out.println("Failed");
+    }
+}
