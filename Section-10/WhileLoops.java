87c185c29055a60d8d4d75ad6220ae7f4697ebf2
alagappan17 <alagappan1703@gmail.com>
2022-08-06 21:10:04 +0530

    While Loop

---
+++ Section-10/WhileLoops.java
@@ -0,0 +1,9 @@
+public class WhileLoops {
+    public static void main(String args[]){
+        int number = 10;
+        while(number > 4){
+            System.out.println(number);
+            number--;
+        }
+    }
+}
